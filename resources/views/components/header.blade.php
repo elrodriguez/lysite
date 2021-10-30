faf86a9f1ce75e02b9ab0ece172f09fa1bcd4165
Elmer Rodriguez <elrodriguez2423@gmail.com>
2021-10-30 15:08:23 -0500

    inicio de proyecto introduccion de la plantilla

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,104 @@
+@php
+    $path = explode('/', request()->path());
+@endphp
+@if($path[0] == 'login' || $path[0] == 'register' || $path[0] == 'pricing')
+<div id="header" class="mdk-header bg-dark js-mdk-header mb-0" data-effects="waterfall blend-background" data-fixed data-condenses>
+    <div class="mdk-header__content">
+
+        <div data-primary class="navbar navbar-expand-sm navbar-dark bg-dark p-0" id="default-navbar">
+            <div class="container">
+
+                <!-- Navbar Brand -->
+                <a href="{{ route('home') }}" class="navbar-brand">
+                    <img class="navbar-brand-icon" src="{{ url('assets/images/logo/white-60.png') }}" width="30" alt="{{ env('APP_NAME','Laravel') }}">
+                    <span class="d-none d-md-block">{{ env('APP_NAME','Laravel') }}</span>
+                </a>
+
+                <!-- Main Navigation -->
+                <ul class="nav navbar-nav ml-auto d-none d-sm-flex">
+                    <li class="nav-item">
+                        <a href="pricing.html" class="nav-link">Pricing</a>
+                    </li>
+                    <li class="nav-item {{ $path[0] == 'register' ? 'active' : '' }}">
+                        <a href="{{ route('register') }}" class="nav-link">Signup</a>
+                    </li>
+                    <li class="nav-item {{ $path[0] == 'login' ? 'active' : '' }}">
+                        <a href="{{ route('login') }}" class="nav-link">Login</a>
+                    </li>
+                </ul>
+                <!-- // END Main Navigation -->
+
+                <!-- Navbar toggler -->
+                <button class="navbar-toggler navbar-toggler-right d-block d-md-none" type="button" data-toggle="sidebar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+@else
+<div id="header" class="mdk-header bg-dark js-mdk-header mb-0" data-effects="waterfall blend-background" data-fixed data-condenses>
+    <div class="mdk-header__content">
+        <div class="navbar navbar-expand-sm navbar-dark bg-dark pr-0 pr-md-16pt" id="default-navbar" data-primary>
+            <!-- Navbar toggler -->
+            <button class="navbar-toggler navbar-toggler-right d-block d-md-none" type="button" data-toggle="sidebar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Navbar Brand -->
+            <a href="{{ route('home') }}" class="navbar-brand">
+                <img class="navbar-brand-icon mr-0 mr-md-8pt" src="{{ url('assets/images/logo/white-60.png') }}" width="30" alt="{{ env('APP_NAME','Laravel') }}">
+                <span class="d-none d-md-block">{{ env('APP_NAME','Laravel') }}</span>
+            </a>
+            <button class="btn btn-black mr-16pt" data-toggle="modal" data-target="#courses">Courses <i class="material-icons">arrow_drop_down</i></button>
+            <form class="search-form search-form--black search-form-courses d-none d-md-flex" action="library-filters.html">
+                <input type="text" class="form-control" placeholder="What would you like to learn?">
+                <button class="btn" type="submit" role="button"><i class="material-icons">search</i></button>
+            </form>
+            <!-- Main Navigation -->
+            <nav class="nav navbar-nav ml-auto flex-nowrap">
+                @if (Route::has('login'))
+                    @auth
+                        <div class="nav-item dropdown d-none d-sm-flex ml-16pt">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                                <img width="32" height="32" class="rounded-circle" src="assets/images/people/50/guy-3.jpg" alt="student" />
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <div class="dropdown-header"><strong>Instructor</strong></div>
+                                <a class="dropdown-item" href="instructor-dashboard.html">Dashboard</a>
+                                <a class="dropdown-item" href="instructor-courses.html">Courses</a>
+                                <a class="dropdown-item" href="instructor-quizzes.html">Quizzes</a>
+                                <div class="dropdown-divider"></div>
+                                <div class="dropdown-header"><strong>Account</strong></div>
+                                <a class="dropdown-item" href="student-edit-account.html">Edit Account</a>
+                                <a class="dropdown-item" href="student-billing-history.html">Billing</a>
+                                <a class="dropdown-item" href="login.html">Logout</a>
+                            </div>
+                        </div>
+                    @else
+                        <div class="ml-16pt nav-item">
+                            <a href="{{ route('login') }}" class="nav-link">
+                                <i class="material-icons">lock_open</i>
+                                <span class="sr-only">Login</span>
+                            </a>
+                        </div>
+                        {{-- @if (Route::has('register'))
+                            <div class="ml-16pt nav-item">
+                                <a href="login.html" class="nav-link">
+                                    <i class="material-icons">lock_open</i>
+                                    <span class="sr-only">Register</span>
+                                </a>
+                            </div>
+                        @endif --}}
+                        <div class="d-none d-sm-flex nav-item">
+                            <a href="pricing.html" class="btn btn-accent">Get started</a>
+                        </div>
+                    @endauth
+                @endif
+            </nav>
+            <!-- // END Main Navigation -->
+        </div>
+    </div>
+</div>
+@endif
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>{{ env('APP_NAME','Laravel') }}</title>
+
+    <!-- Prevent the demo from appearing in search engines -->
+    <meta name="robots" content="noindex">
+
+    <!-- Perfect Scrollbar -->
+    <link type="text/css" href="{{ url('assets/vendor/perfect-scrollbar.css') }}" rel="stylesheet">
+
+    <!-- Fix Footer CSS -->
+    <link type="text/css" href="{{ url('assets/vendor/fix-footer.css') }}" rel="stylesheet">
+
+    <!-- Material Design Icons -->
+    <link type="text/css" href="{{ url('assets/css/material-icons.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ url('assets/css/material-icons.rtl.css') }}" rel="stylesheet">
+
+    <!-- Font Awesome Icons -->
+    <link type="text/css" href="{{ url('assets/css/fontawesome.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ url('assets/css/fontawesome.rtl.css') }}" rel="stylesheet">
+
+    <!-- Preloader -->
+    <link type="text/css" href="{{ url('assets/css/preloader.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ url('assets/css/preloader.rtl.css') }}" rel="stylesheet">
+
+    <!-- App CSS -->
+    <link type="text/css" href="{{ url('assets/css/app.css') }}" rel="stylesheet">
+    <link type="text/css" href="{{ url('assets/css/app.rtl.css') }}" rel="stylesheet">
+</head>
+
+<body class="layout-navbar-mini-fixed-bottom">
+    <div class="preloader">
+        <div class="sk-double-bounce">
+            <div class="sk-child sk-double-bounce1"></div>
+            <div class="sk-child sk-double-bounce2"></div>
+        </div>
+    </div>
+
+    <!-- Header Layout -->
+    <div class="mdk-header-layout js-mdk-header-layout">
+
+        <!-- Header -->
+        <x-header></x-header>
+        <!-- // END Header -->
+        <!-- Header Layout Content -->
+        <div class="mdk-header-layout__content page-content ">
+            {{ $jumbotron }}
+            
+            {{ $slot }}
+
+            <x-footer></x-footer>
+        </div>
+        <!-- // END Header Layout Content -->
+    </div>
+    <!-- // END Header Layout -->
+    {{ $navigation }}
+
+    <!-- jQuery -->
+    <script src="{{ url('assets/vendor/jquery.min.js') }}"></script>
+
+    <!-- Bootstrap -->
+    <script src="{{ url('assets/vendor/popper.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/bootstrap.min.js') }}"></script>
+
+    <!-- Perfect Scrollbar -->
+    <script src="{{ url('assets/vendor/perfect-scrollbar.min.js') }}"></script>
+
+    <!-- DOM Factory -->
+    <script src="{{ url('assets/vendor/dom-factory.js') }}"></script>
+
+    <!-- MDK -->
+    <script src="{{ url('assets/vendor/material-design-kit.js') }}"></script>
+
+    <!-- Fix Footer -->
+    <script src="{{ url('assets/vendor/fix-footer.js') }}"></script>
+
+    <!-- Chart.js -->
+    <script src="{{ url('assets/vendor/Chart.min.js') }}"></script>
+
+    <!-- App JS -->
+    <script src="{{ url('assets/js/app.js') }}"></script>
+
+    <!-- Highlight.js -->
+    <script src="{{ url('assets/js/hljs.js') }}"></script>
+
+    <!-- App Settings (safe to remove) -->
+    <script src="{{ url('assets/js/app-settings.js') }}"></script>
+</body>
+
+</html>
