c1af1f205ae1629526c16031ad09795e6e53388b
Elmer Rodriguez <elrodriguez2423@gmail.com>
2022-03-17 15:03:34 -0500

    cambios en db cuntry

--- database/migrations/2021_11_16_145926_create_departments_table.php
+++ database/migrations/2021_11_16_145926_create_departments_table.php
@@ -20,7 +20,33 @@ class CreateDepartmentsTable extends Migration
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
-
+        DB::table('departments')->insert([
+            ['id' => '01', 'description' => 'AMAZONAS','country_id'],
+            ['id' => '02', 'description' => 'ÁNCASH','country_id'],
+            ['id' => '03', 'description' => 'APURIMAC','country_id'],
+            ['id' => '04', 'description' => 'AREQUIPA','country_id'],
+            ['id' => '05', 'description' => 'AYACUCHO','country_id'],
+            ['id' => '06', 'description' => 'CAJAMARCA','country_id'],
+            ['id' => '07', 'description' => 'CALLAO','country_id'],
+            ['id' => '08', 'description' => 'CUSCO','country_id'],
+            ['id' => '09', 'description' => 'HUANCAVELICA','country_id'],
+            ['id' => '10', 'description' => 'HUÁNUCO','country_id'],
+            ['id' => '11', 'description' => 'ICA','country_id'],
+            ['id' => '12', 'description' => 'JUNÍN','country_id'],
+            ['id' => '13', 'description' => 'LA LIBERTAD','country_id'],
+            ['id' => '14', 'description' => 'LAMBAYEQUE','country_id'],
+            ['id' => '15', 'description' => 'LIMA','country_id'],
+            ['id' => '16', 'description' => 'LORETO','country_id'],
+            ['id' => '17', 'description' => 'MADRE DE DIOS','country_id'],
+            ['id' => '18', 'description' => 'MOQUEGUA','country_id'],
+            ['id' => '19', 'description' => 'PASCO','country_id'],
+            ['id' => '20', 'description' => 'PIURA','country_id'],
+            ['id' => '21', 'description' => 'PUNO','country_id'],
+            ['id' => '22', 'description' => 'SAN MARTIN','country_id'],
+            ['id' => '23', 'description' => 'TACNA','country_id'],
+            ['id' => '24', 'description' => 'TUMBES','country_id'],
+            ['id' => '25', 'description' => 'UCAYALI','country_id'],
+        ]);
     }
 
     /**
--- database/migrations/2022_02_24_085504_add_column_country_department.php
+++ database/migrations/2022_02_24_085504_add_column_country_department.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddColumnCountryDepartment extends Migration
@@ -18,33 +19,9 @@ class AddColumnCountryDepartment extends Migration
             $table->foreign('country_id','departments_country_id_fk')->references('id')->on('countries');
         });
 
-        DB::table('departments')->insert([
-            ['id' => '01', 'description' => 'AMAZONAS','country_id' => 'PE'],
-            ['id' => '02', 'description' => 'ÁNCASH','country_id' => 'PE'],
-            ['id' => '03', 'description' => 'APURIMAC','country_id' => 'PE'],
-            ['id' => '04', 'description' => 'AREQUIPA','country_id' => 'PE'],
-            ['id' => '05', 'description' => 'AYACUCHO','country_id' => 'PE'],
-            ['id' => '06', 'description' => 'CAJAMARCA','country_id' => 'PE'],
-            ['id' => '07', 'description' => 'CALLAO','country_id' => 'PE'],
-            ['id' => '08', 'description' => 'CUSCO','country_id' => 'PE'],
-            ['id' => '09', 'description' => 'HUANCAVELICA','country_id' => 'PE'],
-            ['id' => '10', 'description' => 'HUÁNUCO','country_id' => 'PE'],
-            ['id' => '11', 'description' => 'ICA','country_id' => 'PE'],
-            ['id' => '12', 'description' => 'JUNÍN','country_id' => 'PE'],
-            ['id' => '13', 'description' => 'LA LIBERTAD','country_id' => 'PE'],
-            ['id' => '14', 'description' => 'LAMBAYEQUE','country_id' => 'PE'],
-            ['id' => '15', 'description' => 'LIMA','country_id' => 'PE'],
-            ['id' => '16', 'description' => 'LORETO','country_id' => 'PE'],
-            ['id' => '17', 'description' => 'MADRE DE DIOS','country_id' => 'PE'],
-            ['id' => '18', 'description' => 'MOQUEGUA','country_id' => 'PE'],
-            ['id' => '19', 'description' => 'PASCO','country_id' => 'PE'],
-            ['id' => '20', 'description' => 'PIURA','country_id' => 'PE'],
-            ['id' => '21', 'description' => 'PUNO','country_id' => 'PE'],
-            ['id' => '22', 'description' => 'SAN MARTIN','country_id' => 'PE'],
-            ['id' => '23', 'description' => 'TACNA','country_id' => 'PE'],
-            ['id' => '24', 'description' => 'TUMBES','country_id' => 'PE'],
-            ['id' => '25', 'description' => 'UCAYALI','country_id' => 'PE'],
-        ]);
+        $query = "UPDATE departments SET country_id = 'PE' WHERE 1";
+
+        DB::select($query);
     }
 
     /**
