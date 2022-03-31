d2f97fd5bf1409670cbe258830f2d40ce314b1d6
Grandejosh <grande_josh@odontologos.com>
2022-03-31 01:48:26 -0500

    .
    .

--- Modules/Investigation/Resources/views/livewire/thesis/thesis-parts.blade.php
+++ Modules/Investigation/Resources/views/livewire/thesis/thesis-parts.blade.php
@@ -1,3 +1,69 @@
-<div>
-    <h3>The <code>ThesisParts</code> livewire component is loaded from the <code>Investigation</code> module.</h3>
+<div class="">
+    <div class="container page__container">
+        <ol class="breadcrumb m-0">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">{{ env('APP_NAME','Laravel') }}</a></li>
+            <li class="breadcrumb-item active">{{ __('investigation::labels.thesis_parts') }}</li>
+        </ol>
+    </div>
+    <div class="container page__container">
+        <div class="col-lg-12 p-0 mx-auto">
+            <div class="card card-body mb-32pt">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <h4 class="card-title">Listado</h4>
+                        <p class="text-70">{{ __('investigation::labels.parts') }}</p>
+                        @can('investigacion_partes_nuevo')
+                        <button wire:click="$emit('openModalPartCreate',{{ $format_id }})" type="button" class="btn btn-primary">Nuevo</button>
+                        @endcan
+                    </div>
+                    <div class="col-lg-8 d-flex align-items-center">
+                        <ul class="list-point-none">
+                            @if(count($parts) > 0)
+                                @foreach($parts as $part)
+                                    <li>
+                                        <div class="btn-group mr-3">
+                                            <button wire:click="$emit('openModalPartCreate',{{ $format_id }},{{ $part['id'] }})" type="button" class="btn btn-secondary btn-sm">
+                                                <i class="fa fa-plus"></i>
+                                            </button>
+                                            <button wire:click="$emit('openModalPartEditForm',{{ $part['id'] }})" type="button" class="btn btn-secondary btn-sm">
+                                                <i class="fa fa-pencil-alt"></i>
+                                            </button>
+                                            <button onclick="deletes({{ $part['id'] }})" type="button" class="btn btn-secondary btn-sm">
+                                                <i class="fa fa-trash-alt"></i>
+                                            </button>
+                                        </div>
+                                        {{ $part['number_order'].' '.$part['description'] }}
+                                        {!! $part['items']  !!}
+                                    </li>
+                                @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        function deletes(id){
+            cuteAlert({
+                type: "question",
+                title: "¿Desea eliminar estos datos?",
+                message: "Advertencia:¡Esta acción no se puede deshacer!",
+                confirmText: "Okay",
+                cancelText: "Cancel"
+            }).then((e)=>{
+                if ( e == ("confirm")){
+                    @this.destroy(id)
+                }
+            });
+        }
+        window.addEventListener('inve-part-delete', event => {
+            cuteAlert({
+                type: event.detail.res,
+                title: event.detail.tit,
+                message: event.detail.msg,
+                buttonText: "Okay"
+            });
+        })
+    </script>
 </div>
