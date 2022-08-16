fdee7da36e61940c7e8366fd4c81625dd8fc786e
elrodriguez <elrodriguez2423@gmail.com>
2022-08-15 19:24:27 -0500

    elmer

--- Modules/Investigation/Resources/views/thesis/thesis_export.blade.php
+++ Modules/Investigation/Resources/views/thesis/thesis_export.blade.php
@@ -1,14 +1,20 @@
 @php
 $title = '';
+
 @endphp
 @foreach ($thesis as $thesi)
     <HTML>
     @php
-        $d = $thesis[0]['right_margin'];
-        $l = $thesis[0]['left_margin'];
-        $m = $thesis[0]['between_lines'];
-        $t = $thesis[0]['top_margin'];
-        $b = $thesis[0]['bottom_margin'];
+        // $d = $thesis[0]['right_margin'];
+        // $l = $thesis[0]['left_margin'];
+        // $m = $thesis[0]['between_lines'];
+        // $t = $thesis[0]['top_margin'];
+        // $b = $thesis[0]['bottom_margin'];
+
+        
+        $m = $thesi['between_lines'];
+        $t = $thesi['top_margin'];
+        $b = $thesi['bottom_margin'];
     @endphp
 
     <head>
@@ -18,16 +24,14 @@ $title = '';
                 list-style: none;
             }
 
-            @page {
-                margin-left: {{ $l }}cm;
-                margin-right: {{ $d }}cm;
+            /* @page {
                 margin-top: {{ $t }}cm;
                 margin-bottom: {{ $b }}cm;
-            }
+            } */
         </style>
     </head>
 
-    <body style="padding: 0">
+    <body style="padding: 0;margin:0">
         @if ($title != $thesi['title'])
             {{-- <h1>{{ $thesi['title'] }}</h1> --}}
             <ol>
@@ -35,7 +39,11 @@ $title = '';
                     @if ($key > 0 && $part['salto_de_pagina'])
                         <div class="page-break" style="page-break-after:always;"><span style="display:none;">&nbsp;</span></div>
                     @endif
-                    <li>
+                    @php
+                    $d = $part['right_margin'];
+                    $l = $part['left_margin'];
+                    @endphp
+                    <li style="margin-left: {{ $l }}cm;margin-right: {{ $d }}cm;padding: 0">
                         @if ($part['show_description'])
                             {{ $part['number_order'] . ' ' . $part['description'] }}
                         @endif
