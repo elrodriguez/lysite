165c7b21785e4b748dd6f183cd2a80f5ba2c3478
elrodriguez <elrodriguez2423@gmail.com>
2022-09-02 20:35:29 -0500

    elmer

--- Modules/Homepage/Resources/views/livewire/homepage/histories.blade.php
+++ Modules/Homepage/Resources/views/livewire/homepage/histories.blade.php
@@ -6,116 +6,100 @@
             te apoyamos en desarrollar tu proyecto, contamos con curso online de tesis junto a nuestro sistema de desarrollo asistido de tesis.</p>
     </div>
     <div class="row">
-    
-        <table class="table">
-            <tbody class="">
-    
-                <div class="row">
-                    @foreach ($histories as $key => $history)
-                        <div class="col-sm-6 col-md-4 col-lg-4">
-    
-                            <div class="card card--elevated card-course overlay js-overlay mdk-reveal js-mdk-reveal "
-                                data-partial-height="40" data-toggle="popover" data-trigger="click">
-    
-    
-    
-                                <img src="{{ env('APP_URL') }}/{{ $history->image_path }}" alt="course" style="height: 170px;">
-                                <span class="overlay__content">
-                                    <span class="overlay__action d-flex flex-column text-center">
-                                        <i class="material-icons"><i class="fa fa-university" aria-hidden="true"></i></i>
-                                    </span>
+        @foreach ($histories as $key => $history)
+            <div class="col-sm-6 col-md-4 col-lg-4">
 
-                                </span>
-    
-    
-                                <span
-                                    class="corner-ribbon corner-ribbon--default-right-top corner-ribbon--shadow bg-accent text-white">{{ $history->university }}</span>
-    
-                                <div class="mdk-reveal__content">
-                                    <div class="card-body">
-                                        <div class="d-flex">
-                                            <div class="flex">
-                                                <b class="card-title">{{ $history->title }}</b>
-                                                <small
-                                                    class="text-50 font-weight-bold mb-4pt">{{ __('labels.Institution') }}:
-                                                    {{ $history->university }}</small>
-                                            </div>
-    
-                                        </div>
-                                        <div class="d-flex">
-                                            <small class="text-50">{{ $history->year }}</small>
-                                        </div>
-                                    </div>
+                <div class="card card--elevated card-course overlay js-overlay mdk-reveal js-mdk-reveal "
+                    data-partial-height="40" data-toggle="popover" data-trigger="click">
+
+
+
+                    <img src="{{ env('APP_URL') }}/{{ $history->image_path }}" alt="course" style="height: 170px;">
+                    <span class="overlay__content">
+                        <span class="overlay__action d-flex flex-column text-center">
+                            <i class="material-icons"><i class="fa fa-university" aria-hidden="true"></i></i>
+                        </span>
+
+                    </span>
+
+
+                    <span
+                        class="corner-ribbon corner-ribbon--default-right-top corner-ribbon--shadow bg-accent text-white">{{ $history->university }}</span>
+
+                    <div class="mdk-reveal__content">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex">
+                                    <b class="card-title">{{ $history->title }}</b>
+                                    <small
+                                        class="text-50 font-weight-bold mb-4pt">{{ __('labels.Institution') }}:
+                                        {{ $history->university }}</small>
                                 </div>
+
                             </div>
-                            <div class="popoverContainer d-none">
-                                <div class="media">
-                                    <div class="media-left">
-                                        <img src="{{ env('APP_URL') }}/{{ $history->image_path }}" width="40"
-                                            height="40" alt="Image" class="rounded">
-                                    </div>
-                                    <div class="media-body">
-                                        <div class="card-title mb-0">{{ $history->title }}
-                                        </div>
-                                        <p class="lh-1 mb-0">
-                                            <span class="text-black-50 small">{{ __('labels.Author') }}</span>
-                                            <span
-                                                class="text-black-50 small font-weight-bold">{{ $history->author }}</span>
-                                        </p>
-                                    </div>
-                                </div>
-    
-                                <p class="my-16pt text-black-70">{{ $history->thesis_title }}</p>
-    
-                                <div class="mb-16pt">
-                                    @livewire('homepage::home.details', ['history_id' => $history->id])
-                                </div>
-    
-                                <div class="row align-items-center">
-                                    <div class="col-auto">
-                                        <div class="d-flex align-items-center mb-4pt">
-                                            <span class="material-icons icon-16pt text-black-50 mr-4pt"><i
-                                                    class="fa fa-calendar-alt"></i></span>
-                                            <p class="flex text-black-50 lh-1 mb-0">
-                                                <small>{{ __('labels.' . $history->month) }}
-                                                    {{ $history->year }}</small>
-                                            </p>
-                                        </div>
-    
-                                        <div class="d-flex align-items-center">
-                                            <span class="material-icons icon-16pt text-black-50 mr-4pt"><i
-                                                    class="fa fa-user-graduate"></i></span>
-                                            <p class="flex text-black-50 lh-1 mb-0">
-                                                <small>{{ $history->career }}</small>
-                                            </p>
-                                        </div>
-    
-                                        <div class="d-flex align-items-center">
-                                            <span class="material-icons icon-16pt text-black-50 mr-4pt"><i
-                                                    class="fa fa-university"></i></span>
-                                            <p class="flex text-black-50 lh-1 mb-0">
-                                                <small>{{ $history->university }}</small>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div class="d-flex">
+                                <small class="text-50">{{ $history->year }}</small>
                             </div>
                         </div>
-
-                    @endforeach
-    
+                    </div>
                 </div>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td class="text-end" colspan="3">
-                        <div class="d-flex flex-row-reverse">
-    
+                <div class="popoverContainer d-none">
+                    <div class="media">
+                        <div class="media-left">
+                            <img src="{{ env('APP_URL') }}/{{ $history->image_path }}" width="40"
+                                height="40" alt="Image" class="rounded">
                         </div>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
+                        <div class="media-body">
+                            <div class="card-title mb-0">{{ $history->title }}
+                            </div>
+                            <p class="lh-1 mb-0">
+                                <span class="text-black-50 small">{{ __('labels.Author') }}</span>
+                                <span
+                                    class="text-black-50 small font-weight-bold">{{ $history->author }}</span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <p class="my-16pt text-black-70">{{ $history->thesis_title }}</p>
+
+                    <div class="mb-16pt">
+                        @livewire('homepage::home.details', ['history_id' => $history->id])
+                    </div>
+
+                    <div class="row align-items-center">
+                        <div class="col-auto">
+                            <div class="d-flex align-items-center mb-4pt">
+                                <span class="material-icons icon-16pt text-black-50 mr-4pt"><i
+                                        class="fa fa-calendar-alt"></i></span>
+                                <p class="flex text-black-50 lh-1 mb-0">
+                                    <small>{{ __('labels.' . $history->month) }}
+                                        {{ $history->year }}</small>
+                                </p>
+                            </div>
+
+                            <div class="d-flex align-items-center">
+                                <span class="material-icons icon-16pt text-black-50 mr-4pt"><i
+                                        class="fa fa-user-graduate"></i></span>
+                                <p class="flex text-black-50 lh-1 mb-0">
+                                    <small>{{ $history->career }}</small>
+                                </p>
+                            </div>
+
+                            <div class="d-flex align-items-center">
+                                <span class="material-icons icon-16pt text-black-50 mr-4pt"><i
+                                        class="fa fa-university"></i></span>
+                                <p class="flex text-black-50 lh-1 mb-0">
+                                    <small>{{ $history->university }}</small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        @endforeach
+
     </div>
+
     
 </div>
