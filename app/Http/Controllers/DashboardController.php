9ec14616b0ef97ff52961349ed1446056d3ddb9e
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2022-12-27 18:36:49 -0500

    forzar llenar información de usuario

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,7 +13,12 @@ class DashboardController extends Controller
         if(Auth::user()->hasrole('Admin')){
             return view('dashboard.dashboard_admin');
         }elseif(Auth::user()->hasrole('Student')){
-            return view('dashboard.dashboard_student');
+            if(Person::where('user_id', Auth::user()->id)->exists()){
+                return view('dashboard.dashboard_student');
+            }else{
+                return view('user.edit_information');
+            }
+
         }elseif(Auth::user()->hasrole('Instructor')){
             return view('dashboard.dashboard_instructor');
         }
