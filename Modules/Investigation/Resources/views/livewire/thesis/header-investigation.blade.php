0ba4957d45fbf71c154f17baa722b6d99a2c4e1a
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-02-01 03:50:15 -0500

    d

--- Modules/Investigation/Resources/views/livewire/thesis/header-investigation.blade.php
+++ Modules/Investigation/Resources/views/livewire/thesis/header-investigation.blade.php
@@ -3,8 +3,11 @@
         <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-expanded="false">
             Investigación
         </a>
-      
+
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+            @if (Auth::user()->hasrole('Admin') || Auth::user()->hasrole('Instructor'))
+            <li><a class="dropdown-item" href="{{ route('investigation_thesis_all') }}">Ver todas las tésis</a></li>
+            @endif
             <li><a class="dropdown-item" href="{{ route('investigation_thesis_create') }}">Crear Proyecto</a></li>
             <li><hr class="dropdown-divider"></li>
             @foreach ($thesis as $item)
