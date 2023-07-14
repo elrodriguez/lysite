81d1bf5c1cbdddd7e96b386985963d60c1d02733
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-07-14 04:50:54 -0500

    coments
    -

--- public/assets/js/ckeditor/comments.js
+++ public/assets/js/ckeditor/comments.js
@@ -52,7 +52,7 @@ function buscarYCambiar() {
             range.setEnd(startNode, startOffset + posicion + textoBuscado.length);
             window.getSelection().removeAllRanges();
             window.getSelection().addRange(range);
-            
+
 
             // Obtener el elemento con id "editor"
             var editor = document.getElementById("editor");
@@ -73,83 +73,92 @@ function buscarYCambiar() {
 //setInterval(buscarYCambiar, 1000);
 
 
-function focusComment(thesis_student_id, thesis_format_part_id, selecction_id, selecction_text){
-    let enlace= '/investigation/thesis/parts/'+thesis_student_id+'/'+thesis_format_part_id;
-        enlace = new URL(decodeURIComponent(window.location.href)).origin+enlace;
-        const urlActual = window.location.href;
-        //console.log(enlace, urlActual);
-    if (urlActual.includes(enlace)) {
-                    // Crear un objeto Range que cubra todo el contenido del cuerpo de la página
-                    var range = document.createRange();
-                    range.selectNodeContents(document.body);
+function focusComment(thesis_student_id, thesis_format_part_id, selecction_id, selecction_text) {
+    const urlActual = window.location.href;
 
-                    // Crear un objeto Selection y establecer su rango en el objeto Range creado anteriormente
-                    var sel = window.getSelection();
-                    sel.removeAllRanges();
-                    sel.addRange(range);
+    if (urlActual.includes("investigation/thesis/check")) { // con esto averiguo si estamos en la vista del instructor
+        let ext_id = urlActual.split("/");
+        let enlace = '/investigation/thesis/check/' + ext_id[6] + '/' + thesis_format_part_id;
+        enlace = new URL(decodeURIComponent(window.location.href)).origin + enlace;
+        if (urlActual.includes(enlace)) {
+            search_text(selecction_text);
+        } else {
+            // redirigir a la URL especificada
+            window.location.href = enlace;
+        }
+    } else {
+        let enlace = '/investigation/thesis/parts/' + thesis_student_id + '/' + thesis_format_part_id;
+        enlace = new URL(decodeURIComponent(window.location.href)).origin + enlace;
+        if (urlActual.includes(enlace)) {
+            search_text(selecction_text);
+        } else {
+            // redirigir a la URL especificada
+            window.location.href = enlace;
+        }
+    }
 
-                    // Establecer el punto inicial de la selección en el primer nodo de texto del cuerpo de la página
-                    var startNode = document.body.firstChild;
-                    var startOffset = 0;
-                    while (startNode.nodeType !== Node.TEXT_NODE) {
-                        startNode = startNode.nextSibling;
-                    }
-                    range.setStart(startNode, startOffset);
+}
 
-                    // Establecer el punto final de la selección en el mismo punto que el punto inicial
-                    range.collapse(true);
+function search_text(selecction_text) {
+    // Crear un objeto Range que cubra todo el contenido del cuerpo de la página
+    var range = document.createRange();
+    range.selectNodeContents(document.body);
 
-                    // Establecer el foco en la selección
-                    document.body.focus();
+    // Crear un objeto Selection y establecer su rango en el objeto Range creado anteriormente
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+
+    // Establecer el punto inicial de la selección en el primer nodo de texto del cuerpo de la página
+    var startNode = document.body.firstChild;
+    var startOffset = 0;
+    while (startNode.nodeType !== Node.TEXT_NODE) {
+        startNode = startNode.nextSibling;
+    }
+    range.setStart(startNode, startOffset);
+
+    // Establecer el punto final de la selección en el mismo punto que el punto inicial
+    range.collapse(true);
+
+    // Establecer el foco en la selección
+    document.body.focus();
 
 
-                    var textoBuscado = selecction_text;
+    var textoBuscado = selecction_text;
 
-                    var encontrado = window.find(textoBuscado);
+    var encontrado = window.find(textoBuscado);
 
-                    if (encontrado) {
-                        // El texto se encontró en la página
-                        console.log("Encontrado.");
+    if (encontrado) {
+        // El texto se encontró en la página
+        console.log("Encontrado.");
 
-                        // Seleccionar el texto encontrado
-                        var range = window.getSelection().getRangeAt(0);
-                        var startNode = range.startContainer;
-                        var startOffset = range.startOffset;
-                        var posicion = range.toString().indexOf(textoBuscado);
-                        if (posicion !== -1) {
-                            while (startNode.nodeType !== Node.TEXT_NODE) {
-                                startNode = startNode.childNodes[startOffset];
-                                startOffset = 0;
-                            }
-                            range.setStart(startNode, startOffset + posicion);
-                            range.setEnd(startNode, startOffset + posicion + textoBuscado.length);
-                            window.getSelection().removeAllRanges();
-                            window.getSelection().addRange(range);
-                            
+        // Seleccionar el texto encontrado
+        var range = window.getSelection().getRangeAt(0);
+        var startNode = range.startContainer;
+        var startOffset = range.startOffset;
+        var posicion = range.toString().indexOf(textoBuscado);
+        if (posicion !== -1) {
+            while (startNode.nodeType !== Node.TEXT_NODE) {
+                startNode = startNode.childNodes[startOffset];
+                startOffset = 0;
+            }
+            range.setStart(startNode, startOffset + posicion);
+            range.setEnd(startNode, startOffset + posicion + textoBuscado.length);
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
 
-                            // Obtener el elemento con id "editor"
-                            var editor = document.getElementById("editor");
 
-                            // Desplazar el contenido del elemento con id "editor" hacia abajo en 100 píxeles
-                            editor.scrollTop += 100;
-    
+            // Obtener el elemento con id "editor"
+            var editor = document.getElementById("editor");
 
-                            
-                            
-                            
-                        } else {
-                            console.log("No se pudo encontrar la posición del texto.");
-                        }
-                    } else {
-                        // El texto no se encontró en la página
-                        console.log("El texto no se encontró en la página.");
-                    }
-                    
-      } else {
-        // redirigir a la URL especificada
-        window.location.href = enlace;
-      }
-    
+            // Desplazar el contenido del elemento con id "editor" hacia abajo en 100 píxeles
+            editor.scrollTop += 100;
 
-    
+        } else {
+            console.log("No se pudo encontrar la posición del texto.");
+        }
+    } else {
+        // El texto no se encontró en la página
+        console.log("El texto no se encontró en la página.");
+    }
 }
