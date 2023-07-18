fb101472146d5a652c431afd5cb1c520759224ad
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-07-18 05:27:50 -0500

    mejora de busqueda de texto Chrome. mozilla, EDGE

--- public/assets/js/ckeditor/comments.js
+++ public/assets/js/ckeditor/comments.js
@@ -129,34 +129,46 @@ function search_text(selecction_text) {
     var encontrado = window.find(textoBuscado);
 
     if (encontrado) {
-        // El texto se encontró en la página
-        console.log("Encontrado.");
 
-        // Seleccionar el texto encontrado
-        var range = window.getSelection().getRangeAt(0);
-        var startNode = range.startContainer;
-        var startOffset = range.startOffset;
-        var posicion = range.toString().indexOf(textoBuscado);
-        if (posicion !== -1) {
-            while (startNode.nodeType !== Node.TEXT_NODE) {
-                startNode = startNode.childNodes[startOffset];
-                startOffset = 0;
+        //moviendo los scrools hasta abajo porque parece q asi si funciona 
+        // Obtener todos los elementos con capacidad de scroll
+        var scrollableElements = document.querySelectorAll('*[data-scrollable]');
+
+        // Recorrer los elementos y desplazarlos hasta abajo
+        scrollableElements.forEach(function (element) {
+            element.scrollTop = element.scrollHeight;
+        });
+
+        // Desplazar la página completa hasta abajo
+        document.documentElement.scrollTop = document.documentElement.scrollHeight;
+
+        var searchText = textoBuscado; // El texto que deseas buscar y enfocar
+
+        var textNodes = document.evaluate("//text()", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null); // Obtener todos los nodos de texto en la página
+
+        for (var i = 0; i < textNodes.snapshotLength; i++) {
+            var node = textNodes.snapshotItem(i);
+            var nodeText = node.textContent;
+
+            var startIndex = nodeText.indexOf(searchText);
+            if (startIndex !== -1) {
+                var endIndex = startIndex + searchText.length;
+
+                var range = document.createRange(); // Crear un nuevo rango
+                range.setStart(node, startIndex); // Establecer el punto de inicio del rango
+                range.setEnd(node, endIndex); // Establecer el punto final del rango
+
+                var selection = window.getSelection(); // Obtener la selección actual
+                selection.removeAllRanges(); // Eliminar cualquier rango anterior
+                selection.addRange(range); // Agregar el nuevo rango
+
+                // Desplazar el contenido hacia la selección
+                range.startContainer.parentElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+                break; // Detener la búsqueda después de encontrar la primera coincidencia
             }
-            range.setStart(startNode, startOffset + posicion);
-            range.setEnd(startNode, startOffset + posicion + textoBuscado.length);
-            window.getSelection().removeAllRanges();
-            window.getSelection().addRange(range);
-
-
-            // Obtener el elemento con id "editor"
-            var editor = document.getElementById("editor");
-
-            // Desplazar el contenido del elemento con id "editor" hacia abajo en 100 píxeles
-            editor.scrollTop += 100;
-
-        } else {
-            console.log("No se pudo encontrar la posición del texto.");
         }
+
     } else {
         // El texto no se encontró en la página
         console.log("El texto no se encontró en la página.");
