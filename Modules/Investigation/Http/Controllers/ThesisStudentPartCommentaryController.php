9dafc71d38947d971978bd8a6048c267be37ccad
elrodriguez <elrodriguez2423@gmail.com>
2023-07-22 21:28:30 -0500

    elmer

--- Modules/Investigation/Http/Controllers/ThesisStudentPartCommentaryController.php
+++ Modules/Investigation/Http/Controllers/ThesisStudentPartCommentaryController.php
@@ -6,8 +6,12 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Modules\Investigation\Entities\InveThesisStudentPartSelectionComment;
 use Illuminate\Validation\Validator;
+use Modules\Academic\Emails\NotificationCheckThesisEmail;
+use Modules\Investigation\Entities\InveThesisStudent;
 
 class ThesisStudentPartCommentaryController extends Controller
 {
@@ -31,6 +35,25 @@ class ThesisStudentPartCommentaryController extends Controller
             'commentary' => $request->get('commentary'),
             'user_id' => Auth::id(),
         ]);
+
+        $instructor = DB::table('users')->where('id', Auth::id())->first()->name;
+        $thesis_student = DB::table('inve_thesis_students')->select('people.names', 'inve_thesis_students.external_id')
+            ->join('users', 'users.id', '=', 'inve_thesis_students.user_id')
+            ->join('people', 'people.user_id', '=', 'users.id')
+            ->where('inve_thesis_students.id', $request->get('thesi_student_id'))
+            ->first();
+
+        $name_student = $thesis_student->names;
+        $external_id = $thesis_student->external_id;
+
+        $avatar_url = env('APP_URL') . '/storage/' . Auth::user()->avatar;
+        $correo = new NotificationCheckThesisEmail($instructor, $name_student, $external_id, $request->get('thesi_format_part_id'), $request->get('commentary'), $avatar_url); //$this->question->question_text, $this->answer_text, DB::table('users')->where('id', Auth::id())->first()->name, $this->question_id);
+        $correo->subject = 'Tesis Revisada';
+        $email = DB::table('users')
+            ->join('inve_thesis_students', 'inve_thesis_students.user_id', '=', 'users.id')
+            ->where('inve_thesis_students.external_id', $external_id)->value('email');
+
+        Mail::to($email)->send($correo);
 
         return response()->json([
             'status' => 'success',
