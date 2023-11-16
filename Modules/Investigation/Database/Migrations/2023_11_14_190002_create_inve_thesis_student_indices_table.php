0231c96625c918a926952779fb34d93fa4f4caf7
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-11-16 02:46:21 -0500

    -
    -

--- Modules/Investigation/Database/Migrations/2023_11_14_190002_create_inve_thesis_student_indices_table.php
+++ Modules/Investigation/Database/Migrations/2023_11_14_190002_create_inve_thesis_student_indices_table.php
@@ -15,7 +15,7 @@ class CreateInveThesisStudentIndicesTable extends Migration
     {
         Schema::create('inve_thesis_student_indices', function (Blueprint $table) {
             $table->id();
-            $table->integer('type', 1)->comment('0=general,1=tablas,2=imagen');
+            $table->integer('type')->comment('0=general,1=tablas,2=imagen');
             $table->unsignedBigInteger('thesis_id');
             $table->string('prefix', 6)->nullable();
             $table->string('content', 400)->nullable();
