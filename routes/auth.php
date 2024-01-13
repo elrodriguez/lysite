d63e5336f27c4c81bdeec40fee3a7ed03257f259
Grandejosh <grande_josh@odontologos.com>
2024-01-12 21:49:26 -0500

    Revert "logout repetido cambie 1 a logout2"
    This reverts commit 4692539217fca81ef926ef76d4bd50d8ef9732ae.

--- routes/auth.php
+++ routes/auth.php
@@ -61,9 +61,9 @@ Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
 Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
                 ->middleware('auth');
 
-Route::post('/logout2', [AuthenticatedSessionController::class, 'destroy'])
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
-                ->name('logout2');
+                ->name('logout');
 
 //
 
