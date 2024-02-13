81593a46c93307643d19ad4d8af828f1779ba5e8
Elmer Rodriguez <elrodriguez2423@gmail.com>
2024-02-13 10:42:40 -0500

    el

--- resources/views/livewire/help-gpt/box-gpt.blade.php
+++ resources/views/livewire/help-gpt/box-gpt.blade.php
@@ -1,4 +1,4 @@
-<div class="gpt-container" style="margin-top: 20px">
+<div class="container" style="margin-top: 20px">
     <h3 class="text-center">AYUDA</h3>
     <div class="gpt-messaging">
         <div class="gpt-inbox_msg">
