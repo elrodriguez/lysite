eae846ff610d2d6722288322dad7e4eae569f8eb
José Carlos <grande_josh@odontologos.com>
2024-03-11 00:22:39 -0500

    d
    d

--- resources/views/layouts/lyontech.blade.php
+++ resources/views/layouts/lyontech.blade.php
@@ -12,7 +12,7 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <title>LyONtEHCH</title>
+    <title>Lyonteach</title>
 
     <!-- slider stylesheet -->
     <!-- slider stylesheet -->
