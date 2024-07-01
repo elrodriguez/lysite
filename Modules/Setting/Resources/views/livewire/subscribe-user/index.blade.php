c8578b33a6f0b0db9c571cd58e6917def5e2e2f2
Elmer Rodriguez <elrodriguez2423@gmail.com>
2024-07-01 10:42:21 -0500

    el

--- Modules/Setting/Resources/views/livewire/subscribe-user/index.blade.php
+++ Modules/Setting/Resources/views/livewire/subscribe-user/index.blade.php
@@ -48,8 +48,8 @@
                                                         type="button" class="btn btn-info btn-sm"><i
                                                             class="fa fa-pencil-alt"></i></a>
 
-                                                    <button onclick="deletes({{ $person->type_subscription_id }})" type="button"
-                                                        class="btn btn-danger btn-sm"><i
+                                                    <button onclick="deletes({{ $person->type_subscription_id }})"
+                                                        type="button" class="btn btn-danger btn-sm"><i
                                                             class="fa fa-trash-alt"></i></button>
 
                                                 </div>
