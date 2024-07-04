604c9188d38755e6ce99c37171d9c1fa66b51ebe
Yisus Web <connexion.jesus@gmail.com>
2024-07-04 03:10:35 -0500

    dff

--- resources/views/ly_tarjeta.blade.php
+++ resources/views/ly_tarjeta.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
     <body class="img js-fullheight" style="background-image: url({{ asset('theme-lyontech/images/fondo-naranja.jpg') }});">
-        <div class="container-fluid">
+        <div class="container-section-pagar">
             <div class="row align-content-center justify-content-center">
                 <div class="col-lg-7 col-md-7 col-sm-6">
                     <div class="container-fluid ">
