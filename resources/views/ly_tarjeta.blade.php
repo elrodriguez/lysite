7363f811a52f8e1814c0d2bbba551b965bef9711
Yisus Web <connexion.jesus@gmail.com>
2024-07-04 03:14:23 -0500

    fty

--- resources/views/ly_tarjeta.blade.php
+++ resources/views/ly_tarjeta.blade.php
@@ -5,9 +5,10 @@
     <script src="https://sdk.mercadopago.com/js/v2"></script>
 @stop
 @section('content')
-
-    <body class="img js-fullheight" style="background-image: url({{ asset('theme-lyontech/images/fondo-naranja.jpg') }});">
+<div class="img js-fullheight" style="background-image: url({{ asset('theme-lyontech/images/fondo-naranja.jpg') }});">
+    <body>
         <div class="container-section-pagar">
+            <br>
             <div class="row align-content-center justify-content-center">
                 <div class="col-lg-7 col-md-7 col-sm-6">
                     <div class="container-fluid ">
@@ -140,4 +141,5 @@
             </script>
         @endif
     </body>
+</div>
 @stop
