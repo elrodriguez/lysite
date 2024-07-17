af0628e25e0f1fd1885d9b870d3403c41928b8c7
Yisus Web <connexion.jesus@gmail.com>
2024-07-17 17:09:12 -0500

    oik

--- resources/views/livewire/auth/ly-login-form.blade.php
+++ resources/views/livewire/auth/ly-login-form.blade.php
@@ -20,12 +20,10 @@
                                                         <strong>-</strong>
                                                     </p>
                                             @else
-                                                    <!--
                                                     <p class="mt-0 mb-0  text-center" style="color: #000; font-size: 21px;">
                                                         <strong>S/ {{ $modo->price }}</strong>
                                                         <strong>ó</strong>
                                                     </p>
-                                                    -->
                                                     <p class="mt-0 mb-0  text-center" style="color: #000; font-size: 18px;">
                                                         <strong>$USD {{ $modo->dollar_price }}</strong>
                                                     </p>
