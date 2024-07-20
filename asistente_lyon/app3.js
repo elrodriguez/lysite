6d86c8c0f932427af6bbc7fdd2f948fecfdb5472
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2024-07-20 00:23:20 -0500

    s

--- asistente_lyon/app3.js
+++ asistente_lyon/app3.js
@@ -173,6 +173,7 @@ const createRun = async (data) => {
                         "Recuerda solo limitarte a responder en el contexto creado en el Thread con id: '"+data.thread_id+ ", o la pregunta que te acaban de hacer " +
                         "puedes asistir respondiendo a preguntas y consultas libres siempre que sean  del ámbito de investigación científica de literatura científica, de trabajos académicos superiores y similares; " +
                         "'de la misma manera para mensajes y archivos no respondas ni des información sobre mensajes o archivos de otro thread que no sea este: "+data.thread_id,
+                        tools: [{ type: "file_search" }],
     });
 
     await new Promise((resolve) => setTimeout(resolve, 500));
