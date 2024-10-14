3dfb385009ddd196b07b0f6d86f1467ce00b55f3
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2024-10-14 17:39:20 -0500

    d
    d

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -122,9 +122,7 @@
                     </div>
                     <div class="col-md-8 d-flex justify-content-center align-items-center" style="padding: 40px;">
                         <div class="card-new bg-gradient">
-                            <div style="width: 100%" controls muted playsinline>
-                                <iframe src="https://player.vimeo.com/video/1019497297?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="LYONTEACH VIDEO PRESENTACION FINAL"></iframe>
-                            </div>
+                            <div style="padding:61.88% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1019497297?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="LYONTEACH VIDEO PRESENTACION FINAL"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
                         </div>
                     </div>
                 </div>
